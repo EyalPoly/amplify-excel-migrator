79c8b58cdf98fceb08cab1487014039847cd73fd
eyalpolitansky <10eyal10@gmail.com>
2025-12-16 16:17:17 +0200

    Bump version to 1.3.0 for schema export feature [skip bump]

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="amplify-excel-migrator",
-    version="1.2.16",
+    version="1.3.0",
     author="Eyal Politansky",
     author_email="10eyal10@gmail.com",
     description="A CLI tool to migrate Excel data to AWS Amplify",
